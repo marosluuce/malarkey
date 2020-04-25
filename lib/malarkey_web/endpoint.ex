9f4b77e2aed4da939d1d9f34513c852d5f38e90b
Nathan Walker <earthworn@gmail.com>
2020-04-25 11:42:12 -0400

    Add user authentication.

--- lib/malarkey_web/endpoint.ex
+++ lib/malarkey_web/endpoint.ex
@@ -50,5 +50,6 @@ defmodule MalarkeyWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :malarkey
   plug MalarkeyWeb.Router
 end
--- lib/malarkey_web/router.ex
+++ lib/malarkey_web/router.ex
@@ -1,5 +1,6 @@
 defmodule MalarkeyWeb.Router do
   use MalarkeyWeb, :router
+  use Pow.Phoenix.Router
 
   pipeline :browser do
     plug :accepts, ["html"]
@@ -13,8 +14,19 @@ defmodule MalarkeyWeb.Router do
     plug :accepts, ["json"]
   end
 
-  scope "/", MalarkeyWeb do
+  pipeline :protected do
+    plug Pow.Plug.RequireAuthenticated,
+      error_handler: Pow.Phoenix.PlugErrorHandler
+  end
+
+  scope "/" do
     pipe_through :browser
+
+    pow_routes()
+  end
+
+  scope "/", MalarkeyWeb do
+    pipe_through [:browser, :protected]
 
     get "/", PageController, :index
   end
