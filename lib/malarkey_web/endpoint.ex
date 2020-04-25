79f6b425c41e5e7bc394fa68c94dfa08e277b92c
Nathan Walker <earthworn@gmail.com>
2020-04-25 11:03:03 -0400

    Deploy to Heroku.

--- lib/malarkey_web/endpoint.ex
+++ lib/malarkey_web/endpoint.ex
@@ -11,7 +11,7 @@ defmodule MalarkeyWeb.Endpoint do
   ]
 
   socket "/socket", MalarkeyWeb.UserSocket,
-    websocket: true,
+    websocket: [timeout: 45_000],
     longpoll: false
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
