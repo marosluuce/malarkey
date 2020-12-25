c3e2410daa5ac20e7dba336d52adf2be47091c70
Nathan Walker <earthworn@gmail.com>
2020-12-25 17:38:06 -0500

    Update mix.exs.

--- mix.exs
+++ mix.exs
@@ -33,19 +33,19 @@ defmodule Malarkey.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.1"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:phoenix, "~> 1.5.7"},
+      {:phoenix_ecto, "~> 4.2.1"},
+      {:ecto_sql, "~> 3.5.3"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
-      {:telemetry_metrics, "~> 0.4"},
-      {:telemetry_poller, "~> 0.4"},
-      {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"},
-      {:pow, "~> 1.0"}
+      {:phoenix_html, "~> 2.14.3"},
+      {:phoenix_live_reload, "~> 1.3", only: :dev},
+      {:phoenix_live_dashboard, "~> 0.2.10"},
+      {:telemetry_metrics, "~> 0.6.0"},
+      {:telemetry_poller, "~> 0.5.1"},
+      {:gettext, "~> 0.18.2"},
+      {:jason, "~> 1.2.2"},
+      {:plug_cowboy, "~> 2.4.1"},
+      {:pow, "~> 1.0.21"}
     ]
   end
 
